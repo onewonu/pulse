0fbd719d426663beba8030bebc0b8584af246672
Wonwoo-Seo <so222234@gmail.com>
2025-12-22 23:42:50 +0900

    Odsay searchStation - API 응답

---
+++ src/main/java/com/pulse/dto/StationSearchResult.java
@@ -0,0 +1,65 @@
+package com.pulse.dto;
+
+import java.util.List;
+
+public class StationSearchResult {
+
+    private final int totalCount;
+    private final List<StationItem> stations;
+
+    public StationSearchResult(int totalCount, List<StationItem> stations) {
+        this.totalCount = totalCount;
+        this.stations = stations;
+    }
+
+    public int getTotalCount() {
+        return totalCount;
+    }
+
+    public List<StationItem> getStations() {
+        return stations;
+    }
+
+    public static class StationItem {
+
+        private final String stationName;
+        private final String stationID;
+        private final Double x;
+        private final Double y;
+        private final String laneName;
+
+        public StationItem(
+                String stationName,
+                String stationID,
+                Double x,
+                Double y,
+                String laneName
+        ) {
+            this.stationName = stationName;
+            this.stationID = stationID;
+            this.x = x;
+            this.y = y;
+            this.laneName = laneName;
+        }
+
+        public String getStationName() {
+            return stationName;
+        }
+
+        public String getStationID() {
+            return stationID;
+        }
+
+        public Double getX() {
+            return x;
+        }
+
+        public Double getY() {
+            return y;
+        }
+
+        public String getLaneName() {
+            return laneName;
+        }
+    }
+}
