7fd38d4b4bc6b919f1acf7b9469d6f7b139bd28e
Wonwoo-Seo <so222234@gmail.com>
2025-12-22 22:44:44 +0900

    Odsay searchStation - 설정(properties 주입)

---
+++ src/main/java/com/pulse/config/OdsayApiProperties.java
@@ -0,0 +1,37 @@
+package com.pulse.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationProperties(prefix = "odsay-api")
+public class OdsayApiProperties {
+
+    private String baseUrl;
+    private String key;
+    private int displayCount;
+
+    public String getBaseUrl() {
+        return baseUrl;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public int getDisplayCount() {
+        return displayCount;
+    }
+
+    public void setBaseUrl(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public void setDisplayCount(int displayCount) {
+        this.displayCount = displayCount;
+    }
+}
