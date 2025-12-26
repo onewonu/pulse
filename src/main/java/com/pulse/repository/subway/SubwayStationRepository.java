d6dea2ffc2e6af01c19bf9d779d494a7775a8a83
Wonwoo-Seo <so222234@gmail.com>
2025-12-26 14:50:04 +0900

    data.go.kr(seoul metro schedule) - 지하철 역 리포지토리 수정(stationName like 조회)
     - LIKE CONCAT(:stationName, '%')
       - 역이름에 괄호로 부가 설명이 있는 경우를 찾아 조회하기 위함
       - ex) 공릉(서울과학기술대)

--- src/main/java/com/pulse/repository/subway/SubwayStationRepository.java
+++ src/main/java/com/pulse/repository/subway/SubwayStationRepository.java
@@ -2,12 +2,15 @@ package com.pulse.repository.subway;
 
 import com.pulse.entity.subway.SubwayStation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
 public interface SubwayStationRepository extends JpaRepository<SubwayStation, String> {
 
-    Optional<SubwayStation> findByStationName(String stationName);
+    @Query("SELECT s FROM SubwayStation s WHERE s.stationName LIKE CONCAT(:stationName, '%') ORDER BY LENGTH(s.stationName)")
+    List<SubwayStation> findByStationNameStartingWith(@Param("stationName") String stationName);
 }
