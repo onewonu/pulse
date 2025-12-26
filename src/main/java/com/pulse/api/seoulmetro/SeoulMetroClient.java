e473a4d9b602d255096c29fc29eeac1021d02246
Wonwoo-Seo <so222234@gmail.com>
2025-12-26 14:39:19 +0900

    data.go.kr(seoul metro schedule) - 클라이언트 호출 클래스 생성

---
+++ src/main/java/com/pulse/api/seoulmetro/SeoulMetroClient.java
@@ -0,0 +1,66 @@
+package com.pulse.api.seoulmetro;
+
+import com.pulse.api.seoulmetro.dto.SeoulMetroTrainScheduleResponse;
+import com.pulse.api.seoulmetro.validator.SeoulMetroApiResponseValidator;
+import com.pulse.config.SeoulMetroApiProperties;
+import com.pulse.exception.dataload.ApiCommunicationException;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@Component
+public class SeoulMetroClient {
+
+    private final RestTemplate restTemplate;
+    private final SeoulMetroApiResponseValidator validator;
+    private final SeoulMetroApiProperties properties;
+
+    public SeoulMetroClient(
+            RestTemplate restTemplate,
+            SeoulMetroApiResponseValidator validator,
+            SeoulMetroApiProperties properties
+    ) {
+        this.restTemplate = restTemplate;
+        this.validator = validator;
+        this.properties = properties;
+    }
+
+    public SeoulMetroTrainScheduleResponse getTrainSchedule(
+            String lineName,
+            String stationName,
+            String updownType,
+            String dayType,
+            String tmprTmtblYn
+    ) {
+        String url = buildUrl(lineName, stationName, updownType, dayType, tmprTmtblYn);
+
+        try {
+            SeoulMetroTrainScheduleResponse response = restTemplate.getForObject(url, SeoulMetroTrainScheduleResponse.class);
+            return validator.validate(response);
+        } catch (RestClientException e) {
+            throw new ApiCommunicationException("Failed to communicate with Seoul Metro API: " + url, e);
+        }
+    }
+
+    private String buildUrl(
+            String lineName,
+            String stationName,
+            String updownType,
+            String dayType,
+            String tmprTmtblYn
+    ) {
+        return UriComponentsBuilder.fromUriString(properties.getBaseUrl())
+                .queryParam("serviceKey", properties.getKey())
+                .queryParam("pageNo", 1)
+                .queryParam("numOfRows", properties.getNumOfRows())
+                .queryParam("dataType", properties.getDataType())
+                .queryParam("lineNm", lineName)
+                .queryParam("stnNm", stationName)
+                .queryParam("upbdnbSe", updownType)
+                .queryParam("wkndSe", dayType)
+                .queryParam("tmprTmtblYn", tmprTmtblYn)
+                .build()
+                .toUriString();
+    }
+}
