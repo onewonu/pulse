264943c302c81dc68502a4bea38f5eec76e5819d
Wonwoo-Seo <so222234@gmail.com>
2026-01-09 14:58:57 +0900

    UserDetailsService 자동 생성 제외
    - Spring Boot의 Auto Configuration이 여전히 UserDetailsService를 자동 생성하여 명시적으로 제외

--- src/main/java/com/pulse/PulseApplication.java
+++ src/main/java/com/pulse/PulseApplication.java
@@ -2,12 +2,13 @@ package com.pulse;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableJpaAuditing
 @EnableScheduling
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class PulseApplication {
 
     public static void main(String[] args) {
