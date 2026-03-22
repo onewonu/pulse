0868a472576c1082ac69dfb2c43b68fe5433a7a3
Wonwoo-Seo <so222234@gmail.com>
2026-03-22 15:31:57 +0900

    test: LocalDateTime, LocalTime, LocalDate가 포함된 응답 스니펫이 문자열 대신 배열로 출력 되는 문제 해결

--- src/test/java/com/pulse/support/RestDocsSupport.java
+++ src/test/java/com/pulse/support/RestDocsSupport.java
@@ -1,7 +1,11 @@
 package com.pulse.support;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.restdocs.mockmvc.MockMvcRestDocumentation;
@@ -34,8 +38,13 @@ public abstract class RestDocsSupport {
                 Preprocessors.preprocessResponse(Preprocessors.prettyPrint())
         );
 
+        ObjectMapper objectMapper = new ObjectMapper()
+                .registerModule(new JavaTimeModule())
+                .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
         this.mockMvc = MockMvcBuilders
                 .standaloneSetup(controller())
+                .setMessageConverters(new MappingJackson2HttpMessageConverter(objectMapper))
                 .apply(documentationConfiguration(provider))
                 .alwaysDo(restDocs)
                 .build();
